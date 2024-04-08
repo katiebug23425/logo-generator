2e45304d17fc7a50b08199d512e49a1b50a6ba0d
Katie Bug <katierosealford@gmail.com>
2024-04-08 16:51:02 -0400

    installed jest and fixed import as well as test color criteria. tests are passing. yay!!!

--- Lib/shapes.test.js
+++ Lib/shapes.test.js
@@ -1,11 +1,11 @@
-const {Circle, Square, Triangle,} = require('./Lib/shapes');
+const {Circle, Square, Triangle,} = require('./shapes');
 
 describe('Circle', () => {
     it('should render properly', () => {
       const shape = new Circle();
       var color =('green')
       shape.setColor(color);
-      expect(shape.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="${this.color}"/>`);
+      expect(shape.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="${color}"/>`);
     });
   });
 
@@ -23,7 +23,7 @@ describe('Circle', () => {
       const shape = new Triangle();
       var color =('blue')
       shape.setColor(color);
-      expect(shape.render()).toEqual(`<polygon points="150, 18 244, 182 56, 182" fill="${this.color}"/>`);
+      expect(shape.render()).toEqual(`<polygon points="150, 18 244, 182 56, 182" fill="${color}"/>`);
     });
   });
   
